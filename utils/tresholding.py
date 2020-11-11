10aaa059b63587c1f99455acd65e28850b2f5ea8
Andrew <acidhills@outlook.com>
2020-11-11 16:27:38 +0300

    tresholding

---
+++ utils/tresholding.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+
+def abs_sobel_thresh(img, orient='x', thresh=(0, 255),sobel_kernel=3,):
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sobel = None
+    if(orient == 'x'):
+        sobel = cv2.Sobel(img, cv2.CV_64F, 1, 0,ksize=sobel_kernel)
+    else:
+        sobel = cv2.Sobel(img, cv2.CV_64F, 0, 1,ksize=sobel_kernel)
+    abs_sobel = np.absolute(sobel)
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+    return sxbinary
+
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    magnitude = np.sqrt(sobelx**2 + sobely**2)
+    
+    scaled_sobel = np.uint8(255*magnitude/np.max(magnitude))
+    
+    binary = np.zeros_like(scaled_sobel)
+    binary[(scaled_sobel >= mag_thresh[0]) & (scaled_sobel <= mag_thresh[1])] = 1
+    return binary
+
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):    
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sobelx = np.absolute(cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
+    sobely = np.absolute(cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
+    direction = np.arctan2(sobely, sobelx)
+    
+    binary = np.zeros_like(direction)
+    binary[(direction >= thresh[0]) & (direction <= thresh[1])] = 1
+    return binary
+
+def get_tresholded_img(image):
+    ksize = 3 
+    
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    l_channel = hls[:,:,1]
+    s_channel = hls[:,:,2]
+    gradx = abs_sobel_thresh(s_channel, orient='x', sobel_kernel=ksize, thresh=(20, 100))
+    grady = abs_sobel_thresh(s_channel, orient='y', sobel_kernel=ksize, thresh=(20, 100))
+    dir_binary = dir_threshold(l_channel, sobel_kernel=ksize, thresh=(0.7, 1.3))
+
+    mag_binary = mag_thresh(s_channel, sobel_kernel=ksize, mag_thresh=(30, 100))
+    combined = dir_binary * mag_binary * gradx * grady
+    
+    
+#     combined = gradx
+#     combined = grady
+#     combined = mag_binary
+#     combined = dir_binary
+    return combined
